9e9cad10ab93342034048c199ef268302d159efe
Denis Alayza <denis.alayza@gmail.com>
2020-10-17 18:18:00 -0300

    console data table

---
+++ javascript/aula3/exibirDados.js
@@ -0,0 +1,3 @@
+var nome = prompt('Por favor insira seu nome: ');
+
+console.log(nome);
---
+++ javascript/aula3/usuarioDados.js
@@ -0,0 +1,7 @@
+var nome = prompt('Por favor insira seu nome: ');
+
+var sobrenome = prompt('Por favor insira seu sobrenome: ');
+
+var idade = prompt('Por favor insira seu idade: ');
+
+console.log(`${nome} ${sobrenome} tem ${idade} anos!`);
