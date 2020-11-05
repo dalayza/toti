16e20362f10ff2b4e5fb4aa215147d6c63f658e6
Denis Alayza <denis.alayza@gmail.com>
2020-11-05 03:24:38 -0300

    add component produtos

--- angular/aula_4/helloworld/src/app/app.module.ts
+++ angular/aula_4/helloworld/src/app/app.module.ts
@@ -3,12 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { ProdutosModule } from './produtos/produtos.module';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ProdutosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
